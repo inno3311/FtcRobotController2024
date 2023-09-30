7f034c21b0283006fedc75b31594eb2734d0cb39
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2023-09-30 10:52:35 -0400

    Save

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags/DriveToTag.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTags/DriveToTag.java
@@ -1,20 +1,15 @@
 package org.firstinspires.ftc.teamcode.AprilTags;
 
-import static java.lang.Thread.sleep;
-
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.teamcode.Controller.MechanicalDriveBase;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 public class DriveToTag
 {
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Prototype/PrototypeMaster.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.Prototype;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Controller.MechanicalDriveBaseGoBILDA;
+
+@TeleOp(name = "Prototype TeleOp", group = "full code")
+public class PrototypeMaster extends OpMode
+{
+    MechanicalDriveBaseGoBILDA mechanicalDriveBaseGoBILDA;
+    ElapsedTime elapsedTime;
+
+    @Override
+    public void init()
+    {
+        mechanicalDriveBaseGoBILDA = new MechanicalDriveBaseGoBILDA(hardwareMap);
+        telemetry.addData("Ready", "");
+        telemetry.update();
+    }
+
+    @Override
+    public void loop()
+    {
+        telemetryCall();
+
+    }
+
+    private void telemetryCall()
+    {
+        mechanicalDriveBaseGoBILDA.driveBaseTelemetry(telemetry);
+    }
+}
