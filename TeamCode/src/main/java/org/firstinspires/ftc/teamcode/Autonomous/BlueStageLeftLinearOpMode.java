4c7e8f34b5c92302f09af70e9a26e2859df3921d
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2023-10-28 12:50:08 -0400

    Yay, i added a few lines of code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueStageLeftLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueStageLeftLinearOpMode.java
@@ -16,28 +16,26 @@ import java.io.IOException;
 @Autonomous(name = "Blue Stage Left", group = "Group3311")
 public class BlueStageLeftLinearOpMode extends LeftRightSuper
 {
-    private boolean pixelInMiddle, pixelIsLeft, pixelIsRight;
 
-    BlueStageRightLinearOpMode blueStageRightLinearOpMode;
-
-    WebCamHardware webcam;
-
-    ImuHardware imuControl;
-
-    /** Drive control */
-    MecanumSynchronousDriver driver;
-    AprilTagMaster aprilTagMaster;
-    InitAprilTags initAprilTags;
-    DriveToTag driveToTag;
-    private final double ticksPerInch = (8192 * 1) / (2 * 3.1415); // == 1303
-    private final double ticksPerDegree = (ticksPerInch * 50.24) / 360;
-    enum zone
-    {
-        middle,
-        left,
-        right
-    }
-    private zone current = null;
+//    BlueStageRightLinearOpMode blueStageRightLinearOpMode;
+//
+//    WebCamHardware webcam;
+//
+//    ImuHardware imuControl;
+//
+//    /** Drive control */
+//    MecanumSynchronousDriver driver;
+//    AprilTagMaster aprilTagMaster;
+//    InitAprilTags initAprilTags;
+//    DriveToTag driveToTag;
+//    private final double ticksPerInch = (8192 * 1) / (2 * 3.1415); // == 1303
+//    private final double ticksPerDegree = (ticksPerInch * 50.24) / 360;
+//    enum zone
+//    {
+//        middle,
+//        left,
+//        right
+//    }
 
     @Override
     public void runOpMode() throws InterruptedException
@@ -50,21 +48,66 @@ public class BlueStageLeftLinearOpMode extends LeftRightSuper
             e.printStackTrace();
         }
 
+
+//        int day = 1;
+//        switch (day)
+//        {
+//            case 1:
+//                System.out.println("It's sunday");
+//                break;
+//            case 2:
+//                System.out.println("It's Monday");
+//                break;
+//            default:
+//                System.out.println("There is a problem there are only 7 days in a week numbers is not in range");
+//                break;
+//        }
+
+
         sleep(1000);
         driveToTag.drive(7, zone.ordinal() + 1, 11, 0);
     }
+//    private void left() {
+//
+//    }
+//    private void right() {
+//    }
 
+
+    //I recommend deleting this once you know you don't need it
     public void right()
     {
+
+        driver.forward(20,1,0.6);
         driver.forward(24,1,0.6);
         sleep(500);
-        driver.turn(30,1,0.4);
+        driver.turn(45,1,0.4);
         sleep(500);
-        driver.forward(2,1,0.6);
+        driver.forward(5,1,0.6);
         sleep(500);
-        driver.forward(2,-1,0.6);
-
+        driver.forward(5,-1,0.6);
+        sleep(500);
+        driver.turn(135,-1,0.4);
+        sleep(500);
+        driver.forward(20,1,0.6);
     }
+
+/** As well as this commented-out code
+* */
+//     public void left()
+//    {
+//        driver.forward(20,1,0.6);
+//        sleep(500);
+//        driver.turn(45,-1,0.4);
+//        sleep(500);
+//        driver.forward(9,1,0.6);
+//        sleep(500);
+//        driver.forward(9,-1,0.6);
+//        sleep(500);
+//        driver.turn(135,-1,0.4);
+//        sleep(500);
+//        driver.forward(20,1,0.6);
+//    }
 
     /**
      * Plan Alpha.  You will design different routes based on what intel the other team provides.
@@ -76,8 +119,8 @@ public class BlueStageLeftLinearOpMode extends LeftRightSuper
         //probably need to back up a bit before rotating to your end goal.  I will let you figure
         //out what route that will be.
 
-        //Go forward 24 inches at speed of .5  (24 is just a filler.  you need to figure out how far it is
-        blueStageRightLinearOpMode.planPurple(blueStageRightLinearOpMode.zone, false);
+
+        planPurple(zone, false);
 
         sleep(1000);
 
@@ -85,6 +128,8 @@ public class BlueStageLeftLinearOpMode extends LeftRightSuper
         driver.rotate(-90, imuControl);
         sleep(1000);
 
+        // Turns 90 degrees left
+        driver.turn(90, -1, .4);
 
         sleep(1000);
         //Turn right
@@ -93,25 +138,30 @@ public class BlueStageLeftLinearOpMode extends LeftRightSuper
 
         driver.forward(16, 1, 0.5);
 
-        //Left and let AprilTag take over
-        driver.rotate(-90, imuControl);
-
-
     }
 
     /**
      * There is always a plan B.  ;)
      */
     public void planGamma(SpikeLineEnum zone) throws IOException, InterruptedException {
+
+        //This planPurple detects the object and pushes the pixel toward it. It must go first
+        //You need to write code that goes back and faces the backdrop so that Sam's AprilTag program can take over.
         planPurple(zone, false);
+        //Write your code here
 
+     driver.forward(14.5,1,5.0);
 
+     sleep(1000);
+
+     driver.turn(90,-0,.4);
+
+     sleep(1000);
+
+     driver.forward();
 
 
     }
-
-
-
 
 }
 
