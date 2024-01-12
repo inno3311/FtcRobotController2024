53ebc6d8b338832044f2de62992aaf2bae561b38
Innovators 3311 Robotics FTC <innovators3311@gmail.com>
2024-01-11 20:29:30 -0500

    This Repositoy is now ready

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoCrash/NoCrashMaster.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NoCrash/NoCrashMaster.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonomous.NoCrash;
+package org.firstinspires.ftc.teamcode.NoCrash;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SearchAndFindRadar/RadarCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SearchAndFindRadar/RadarCode.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Autonomous.SearchAndFindRadar;
+package org.firstinspires.ftc.teamcode.SearchAndFindRadar;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
